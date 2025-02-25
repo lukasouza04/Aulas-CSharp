efcaf13fa9e68830a485eccbd0ded28ec9eb4813
lukasouza04 <luka.asouza@senacsp.edu.br>
2025-02-25 20:32:33 -0300

    ,,,

---
+++ aula01/aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Guilherme\n");
+Console.WriteLine("Amaro \n");
+
+string nomeCompleto = "Guilherme Amaro Pereira Rodrigues";
+string primeiroNome = "Guilherme";
+string ultimoNome = "Rodrigues";
+string nomeMeio = "Amaro Pereira";
+Console.WriteLine("Meu nome é " + primeiroNome + nomeMeio + ultimoNome);
+int idade = 26;
+Console.WriteLine($"Meu nome é   {primeiroNome}  {nomeMeio} {ultimoNome} e minha idade é {idade}");
+Console.WriteLine("Escreva o seu Nome  ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Ola {nome}, Boa noite !");
